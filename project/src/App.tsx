358cc7b2e3667d35d81dab39937793e89f363d05
platstarinum-prog <platstarinum@gmail.com>
2026-05-18 17:14:08 +0300

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -9,7 +9,6 @@ import Footer from './components/Footer';
 function App() {
   return (
     <div className="min-h-screen bg-[#050505]">
-      <CursorGlow />
       <Navbar />
       <main>
         <Hero />
